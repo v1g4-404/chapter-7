3656d463bd5cb9943bf0c7cc104ae23d08c25f52
眞野初芽 <manohatsume@manohatsumenoMacBook-Pro.local>
2025-12-13 10:14:00 +0900

    課題2の実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Home from "./pages/Home"
+import Detail from "./pages/Detail"
+import Banner from "./pages/Banner"
+import Contact from "./pages/Contact"
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Banner />
+
+      <Routes>
+        <Route path='/'>
+          <Route index={true} element={<Home />} />
+          <Route path='posts/:id' element={<Detail />} />
+        </Route>
+        <Route path='contact' element={<Contact />} />
+
+      </Routes>
+    </>
   );
 }
 
